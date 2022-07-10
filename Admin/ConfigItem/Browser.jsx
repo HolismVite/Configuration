8c25afc4ae6bebedb90d55a4e523fe265e5462bf
Saeed Nemati <sn.1361@gmail.com>
2022-07-10 18:18:23 +0430

    committed by script

--- Admin/ConfigItem/Browser.jsx
+++ Admin/ConfigItem/Browser.jsx
@@ -1,26 +1,21 @@
 import { Browse as BrowseInput } from '@Form';
 import { Browse } from '@Browse';
 
-const card = (item) => {
+const headers = <>
+    <th superAdmin>Key</th>
+    <th>Name</th>
+    <th>Type</th>
+</>
 
-    return <>
-        <div
-            dir='ltr'
-            className="flex flex-row items-center gap-4 mb-4 "
-        >
-            <div>{item.name}</div>
-        </div>
-        <p
-            dir='ltr'
-            style={{ overflowWrap: 'anywhere' }}
-        >{item.text}</p>
-    </>
-}
+const row = (item) => <>
+    <td>{item.name}</td>
+</>
 
 const BrowserDialog = () => {
     return <Browse
         entityType='ConfigItem'
-        card={card}
+        headers={headers}
+        row={row}
     />
 }
 
