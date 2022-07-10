e2be57ed28438c02d3fb26f62f66b8ff645e6393
Saeed Nemati <sn.1361@gmail.com>
2022-07-10 16:48:11 +0430

    committed by script

--- Admin/SystemConfig/Upsert.jsx
+++ Admin/SystemConfig/Upsert.jsx
@@ -1,38 +1,8 @@
-import { DialogForm, Browse as BrowseInput } from '@Form';
-import { Browse } from '@Browse';
-
-const card = (item) => {
-
-    return <>
-        <div
-            dir='ltr'
-            className="flex flex-row items-center gap-4 mb-4 "
-        >
-            <div>{item.name}</div>
-        </div>
-        <p
-            dir='ltr'
-            style={{ overflowWrap: 'anywhere' }}
-        >{item.text}</p>
-    </>
-}
-
-const ConfigItemBrowser = () => {
-    return <Browse
-        entityType='log'
-        card={card}
-    />
-}
+import { DialogForm } from '@Form';
+import ConfigItemBrowser from '../ConfigItem/Browser';
 
 const inputs = <>
-    <BrowseInput
-        column='configItemId'
-        browser={ConfigItemBrowser}
-        placeholder='Config item'
-        display={(item) => item.name}
-        choose={(item) => item.id}
-        required='You should choose configuration item'
-    />
+    <ConfigItemBrowser />
 </>
 
 const UpsertSystemConfig = () => {
