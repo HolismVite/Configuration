2ee1e9374de6a3d76ac71b5dc106bb391d851fdd
Saeed Nemati <sn.1361@gmail.com>
2022-07-13 18:09:20 +0430

    committed by script

--- Admin/EntityConfigs/Manage.jsx
+++ Admin/EntityConfigs/Manage.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import {
+    Check,
+    Color,
     DialogForm,
     get,
+    Numeric,
+    Select,
     Text,
 } from '@Form'
+import { useEnum } from 'Hooks'
 
 const ManageEntityConfigs = ({
     entityType,
@@ -11,6 +16,8 @@ const ManageEntityConfigs = ({
 }) => {
 
     const [data, setData] = useState([])
+
+    const { enumItems, progress } = useEnum({ entityType: 'ConfigType' })
 
     const load = ({
         error,
@@ -27,11 +34,44 @@ const ManageEntityConfigs = ({
             })
     }
 
+    const getField = (entity) => {
+
+        switch (entity.configTypeId) {
+            case enums?.configType?.text:
+                return <Text
+                    column={entity.configItemId}
+                />
+            case enums?.configType?.naturalNumber:
+            case enums?.configType?.integer:
+            case enums?.configType?.realNumber:
+                return <Numeric
+                    column={entity.configItemId}
+                />
+            case enums?.configType?.boolean:
+            case enums?.configType?.nullableBoolean:
+                return <Check
+                    column={entity.configItemId}
+                />
+            case enums?.configType?.color:
+                return <Color
+                    column={entity.configItemId}
+                />
+            case enums?.configType?.singleChoice:
+            case enums?.configType?.multipleChoice:
+                return <Select
+                    column={entity.configItemId}
+                />
+            case enums?.configType?.percent:
+                return null
+            default:
+                return null
+        }
+    }
+
     const inputs = <>
-        <Text
-            column='Text'
-            placeholder='Text'
-        />
+        {
+            data.map(i => getField(i))
+        }
     </>
 
     return <DialogForm
