fc612be05e42996fc17f356e905bf3755349c1d6
Saeed Nemati <sn.1361@gmail.com>
2022-07-13 15:21:59 +0430

    committed by script

--- Admin/EntityConfigs/Manage.jsx
+++ Admin/EntityConfigs/Manage.jsx
@@ -1,8 +1,11 @@
-import { DialogForm } from '@Form'
+import { useState, useEffect } from 'react'
+import { DialogForm, Text } from '@Form'
 
 const ManageEntityConfigs = ({
     entityGuid
 }) => {
+
+    const [progress, setProgress] = useState(true)
 
     const inputs = <>
         <Text
@@ -10,9 +13,10 @@ const ManageEntityConfigs = ({
             placeholder='Text'
         />
     </>
-    
+
     return <DialogForm
         title='Entity Configs'
+        progress={progress}
         inputs={inputs}
     />
 }
